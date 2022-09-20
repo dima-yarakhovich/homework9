b2a644cffc4251273675719061d418908b6b7c62
dima-yarakhovich <dima020490@tut.by>
2022-09-20 19:05:43 +0300

    hw9.1-9.5

--- src/Main.java
+++ src/Main.java
@@ -5,40 +5,34 @@ public class Main {
 
 
         int[] arr = generateRandomArray();
-        int sum=0;
+        int sum = 0;
 
         for (int i : arr) {
             sum += i;
         }
-        System.out.println("Сумма трат за месяц составила "+sum+ " рублей");
-        int max=Integer.MIN_VALUE;
-        int min=Integer.MAX_VALUE;
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей");
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
         for (int j : arr) {
             if (j > max) {
                 int max1 = j;
                 max = max1;
             }
         }
-            for (int i = 0; i < arr.length; i++) {
-            if (arr[i]<min) {
-                min=arr[i];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
             }
         }
-        System.out.println("Максимальная сумма трат за день составила "+max+ " рублей");
-        System.out.println("Минимальная сумма трат за день составила "+min+ " рублей");
-        double average=sum/30;
-        System.out.println("Средняя сумма трат за месяц составила "+average+ " рублей");
-
-        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-        for (int index = reverseFullName.length-1; index >= 0; index--) {
+        System.out.println("Максимальная сумма трат за день составила " + max + " рублей");
+        System.out.println("Минимальная сумма трат за день составила " + min + " рублей");
+        double average = sum / 30;
+        System.out.println("Средняя сумма трат за месяц составила " + average + " рублей");
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int index = reverseFullName.length - 1; index >= 0; index--) {
             System.out.print(reverseFullName[index]);
         }
-
-
-
     }
-
-
 
     public static int[] generateRandomArray() {
         java.util.Random random = new java.util.Random();
